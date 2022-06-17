3f26f59b6623c6ef6b8a41d2fe063f6bd985c0b5
Orochyy <yura333619@gmail.com>
2022-06-17 15:11:41 +0300

    panic: dial tcp 127.0.0.1:5672: connect: connection refused

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("Go RabbitMQ Tutorial")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println("Failed Initializing Broker Connection")
+		panic(err)
+	}
+
+	// Let's start by opening a channel to our RabbitMQ instance
+	// over the connection we have already established
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+
+	// with this channel open, we can then start to interact
+	// with the instance and declare Queues that we can publish and
+	// subscribe to
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	// We can print out the status of our Queue here
+	// this will information like the amount of messages on
+	// the queue
+	fmt.Println(q)
+	// Handle any errors if we were unable to create the queue
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// attempt to publish a message to the queue!
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Published Message to Queue")
+}
